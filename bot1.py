f7d61f660e40e752f4ae8678761eb809348e5def
luxx-in-tenebris <orotimaru1@mail.ru>
2020-04-04 18:05:48 +0300

    1

---
+++ bot1.py
@@ -0,0 +1,23 @@
+import telebot
+from telebot import apihelper
+import requests
+
+
+bot = telebot.TeleBot('')
+
+@bot.message_handler(content_types=['text'])
+def get_text_messages(message):
+
+    if message.text == "Привет":
+
+        bot.send_message(message.from_user.id, "Привет, сейчас я расскажу тебе гороскоп на сегодня.")
+
+    elif message.text == "/help":
+
+        bot.send_message(message.from_user.id, "Напиши Привет")
+
+    else:
+
+        bot.send_message(message.from_user.id, "Я тебя не понимаю. Напиши /help.")
+
+bot.polling(none_stop=True, interval=0)
