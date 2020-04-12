fda5feac53374b9e5b0a8844ecd0f5a252e29c87
luxx-in-tenebris <orotimaru1@mail.ru>
2020-04-12 15:34:56 +0300

    add ai

--- new1_bot.py
+++ new1_bot.py
@@ -1,6 +1,8 @@
 import telebot
 from telebot import apihelper
 import requests
+import apiai
+import json
 
 TOKEN = '1187232177:AAHwgtMxicM0U9bcd2P4tu2yObjr4CQvDWc'
 def main(): 
@@ -10,30 +12,31 @@ def main():
 
     @bot.message_handler(commands=['start', 'go'])
     def start_handler(message):
-            text=message.text.lower()
+            #text=message.text.lower()
             chat_id=message.chat.id
-            msg=bot.send_message(message.chat.id,'Привет, сколько лет тебе?')
-            bot.register_next_step_handler(msg,askAge)
-    def askAge(message):
-        text=message.text.lower()
-        chat_id=message.chat.id
-        if not text.isdigit():
-            msg=bot.send_message(chat_id,'Возраст должен быть числом, введи ещё раз')
-            bot.register_next_step_handler(msg,askAge)
-            return
-        msg = bot.send_message(chat_id,'Так тебе всего '+text+'лет? Ладно, пойдет')
+            msg=bot.send_message(chat_id,'Салют! Что скажешь?')
         
 
     @bot.message_handler(content_types=['text'])
     def text_handler(message):
         text=message.text.lower()
         chat_id=message.chat.id
-        if text == "привет":
-            bot.send_message(chat_id,'Привет, я бот')
-        elif text =="как дела?":
-            bot.send_message(chat_id,'Хорошо, а у тебя?')
+
+        request=apiai.ApiAI('ac825c1f8d82438092800fc8972e8571').text_request() #токен к dialogflow
+        request.lang='ru'
+        request.session_id='TestSession' #Id Сессии диалога (нужно, чтобы потом учить бота)
+        request.query=text #посылаем запрос к ИИ
+        responseJson=json.loads(request.getresponse().read().decode('utf-8'))
+        response=responseJson['result']['fulfillment']['speech']#разбираем json и вытаскиваем ответ
+
+        #если есть ответ от бота - присылаем юзеру, если нет - бот его не понял
+        if response:
+            bot.send_message(chat_id,responseJson)
         else:
-            bot.send_message(chat_id, 'Не понял')
+            bot.send_message(chat_id,'Не понял, чо?')
+        
+
+
     @bot.message_handler(content_types=['photo','video','sticker'])
     def text_handler1(message):
         stiс=open('stic.webp','rb')
