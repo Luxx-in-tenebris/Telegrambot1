ba991a961e5870054b7ee01768f33668db535d3e
luxx-in-tenebris <orotimaru1@mail.ru>
2020-04-12 21:00:46 +0300

    calendar

--- new1_bot.py
+++ new1_bot.py
@@ -35,7 +35,14 @@ def main():
         else:
             bot.send_message(chat_id,'Не понял, чо?')
         
-
+    @bot.message_handler(commands=['calendar'])
+    def get_calendar(message):
+        now=datetime.datetime.now()
+        chat_id=message.chat.id
+        date=(now.year,now.month)
+        current_shown_dates[chat_id] = date #сохраним текущую дату в словарь
+        markup = create_calendar(now.year,now.month)
+        bot.send_message(chat_id,"Пожалуйста выберите дату",reply_markup=markup)
 
     @bot.message_handler(content_types=['photo','video','sticker'])
     def text_handler1(message):
